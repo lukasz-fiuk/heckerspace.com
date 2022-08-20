24399d5cc02b3064fd5e832a8f7bbdd5ae20dc0a
Łukasz Fiuk <lukasz.fiuk@lepolishbureau.com>
2022-08-20 03:07:53 +0200

    setup: use encrypted keys

--- cms/env/production/server.js
+++ cms/env/production/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   url: env("MY_HEROKU_URL"),
   proxy: true,
   app: {
-    keys: env.array("APP_KEYS", ["testKey1", "testKey2"]),
+    keys: env.array("APP_KEYS"),
   },
 });
