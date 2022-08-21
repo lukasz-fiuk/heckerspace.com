987eb2d94637b41e526010e2269bdbdc130a38a6
Łukasz Fiuk <lukasz.fiuk@lepolishbureau.com>
2022-08-21 02:03:31 +0200

    chore: remove temp params

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,4 +1,3 @@
-import { GetStaticProps } from "next";
 import React, { FC } from "react";
 
 export interface HomeProps {}
@@ -13,39 +12,3 @@ const Home: FC<HomeProps> = ({ ...rest }) => {
   );
 };
 export default Home;
-
-export const getFetchParams = (query: string) => {
-  return {
-    method: "POST",
-    headers: {
-      "content-type": "application/json",
-    },
-    body: JSON.stringify({
-      query: query,
-    }),
-  };
-};
-
-export const getStaticProps: GetStaticProps = async ({ locale }) => {
-  const fetchParams = getFetchParams(`
-  {
-    articles {
-      data {
-        attributes {
-          slug
-        }
-      }
-    }
-  }
-  `);
-
-  const res = await fetch(`${process.env.STRAPIBASEURL}/graphql`, fetchParams);
-  const { data } = await res.json();
-
-  if (!data) return { notFound: true };
-
-  return {
-    props: { data: data },
-    revalidate: 10,
-  };
-};
