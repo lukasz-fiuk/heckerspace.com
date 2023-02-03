c7fe1613d870b9e5f49fd52c6942f3b0b22016c1
Łukasz Fiuk <lukasz.fiuk@lepolishbureau.com>
2023-02-03 21:08:00 +0100

    feat: add linearGradient mixin 🌈

--- src/styled/mixins.ts
+++ src/styled/mixins.ts
@@ -8,3 +8,21 @@ export const zIndex = (
   const order = ["", "aboveContent", "overlay", "header", "absoluteTop"];
   return order.indexOf(id) * 100;
 };
+
+type GradientDirection =
+  | "to right"
+  | "to left"
+  | "to top"
+  | "to bottom"
+  | "to top right"
+  | "to top left"
+  | "to bottom right"
+  | "to bottom left";
+
+export const linearGradient = (
+  fromColor: string,
+  toColor: string,
+  direction: GradientDirection = "to bottom"
+) => `
+  linear-gradient(${direction}, ${fromColor}, ${toColor});
+`;
