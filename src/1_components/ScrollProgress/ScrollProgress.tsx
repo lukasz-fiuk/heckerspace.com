1d4ee31231763c6b93148607dc760971c75ab92f
Łukasz Fiuk <lukasz.fiuk@lepolishbureau.com>
2023-02-04 22:00:53 +0100

    refine: transition opacity for scrollProgress

--- src/1_components/ScrollProgress/ScrollProgress.tsx
+++ src/1_components/ScrollProgress/ScrollProgress.tsx
@@ -14,7 +14,8 @@ const ScrollProgress: FC<ScrollProgressProps> = ({ ...rest }) => {
 
   return (
     <S.ScrollIndicator
-      initial={{ scaleX: 0 }}
+      initial={{ scaleX: 0, opacity: 0 }}
+      animate={{ opacity: 1 }}
       exit={{
         scaleX: [null, 1],
         scaleY: 0,
