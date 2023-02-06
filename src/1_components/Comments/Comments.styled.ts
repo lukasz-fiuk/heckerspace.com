106ddeb45e9110071b66339b93cb30562630a162
Łukasz Fiuk <lukasz.fiuk@lepolishbureau.com>
2023-02-06 01:12:35 +0100

    style: add transition

---
+++ src/1_components/Comments/Comments.styled.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const CommentsWrapper = styled.div`
+  width: 100%;
+
+  & #comments {
+    transition: color 0.3s ease-in-out, background 0.3s ease-in-out;
+  }
+`;
