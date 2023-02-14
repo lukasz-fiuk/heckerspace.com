22682ab372aeb47c63c0ab8973a4a067d2bfdfc2
Łukasz Fiuk <lukasz.fiuk@lepolishbureau.com>
2023-02-14 03:48:47 +0100

    feat: add threshold to "useScrollDirection" hook and prevent overscroll checks

--- src/hooks/useScrollDirection.ts
+++ src/hooks/useScrollDirection.ts
@@ -21,19 +21,33 @@ interface UseScrollDirectionResult {
   isUserScrollingUp: boolean;
 }
 
-const useScrollDirection = (): UseScrollDirectionResult => {
+const useScrollDirection = (threshold = 100): UseScrollDirectionResult => {
   const [direction, setDirection] = useState<Direction>("up");
   const previousScrollYRef = useRef(0);
 
   useEffect(() => {
     const handleScroll = () => {
       const currentScrollY = window.scrollY;
-      if (currentScrollY > previousScrollYRef.current) {
-        setDirection("down");
-      } else {
-        setDirection("up");
+      const scrollDistance = Math.abs(
+        currentScrollY - previousScrollYRef.current
+      );
+      const isAtDocumentBottom =
+        window.innerHeight + currentScrollY >=
+        document.documentElement.scrollHeight;
+      const isAtDocumentTop = currentScrollY === 0;
+
+      if (
+        scrollDistance >= threshold &&
+        !isAtDocumentBottom &&
+        !isAtDocumentTop
+      ) {
+        if (currentScrollY > previousScrollYRef.current) {
+          setDirection("down");
+        } else {
+          setDirection("up");
+        }
+        previousScrollYRef.current = currentScrollY;
       }
-      previousScrollYRef.current = currentScrollY;
     };
 
     window.addEventListener("scroll", handleScroll, { passive: true });
@@ -41,7 +55,7 @@ const useScrollDirection = (): UseScrollDirectionResult => {
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
-  }, []);
+  }, [threshold]);
 
   return { direction, isUserScrollingUp: direction === "up" };
 };
