ad549a70af79f12c47c061aaa26ce324c2b23a9d
Łukasz Fiuk <lukasz.fiuk@lepolishbureau.com>
2023-06-23 02:29:15 +0200

    convert to jsx

--- src/1_components/Svgs/hero/Circles.tsx
+++ src/1_components/Svgs/hero/Circles.tsx
@@ -18,61 +18,61 @@ const Circles: FC<CirclesProps> = ({ className, ...rest }) => {
       <circle
         r="97.5"
         stroke="currentColor"
-        stroke-opacity=".5"
+        strokeOpacity=".5"
         transform="matrix(1 0 0 -1 98 98)"
       />
       <circle
         r="97.5"
         stroke="currentColor"
-        stroke-opacity=".5"
+        strokeOpacity=".5"
         transform="matrix(1 0 0 -1 180.094 98)"
       />
       <circle
         r="97.5"
         stroke="currentColor"
-        stroke-opacity=".5"
+        strokeOpacity=".5"
         transform="matrix(1 0 0 -1 114.419 98)"
       />
       <circle
         r="97.5"
         stroke="currentColor"
-        stroke-opacity=".5"
+        strokeOpacity=".5"
         transform="matrix(1 0 0 -1 196.513 98)"
       />
       <circle
         r="97.5"
         stroke="currentColor"
-        stroke-opacity=".5"
+        strokeOpacity=".5"
         transform="matrix(1 0 0 -1 130.838 98)"
       />
       <circle
         r="97.5"
         stroke="currentColor"
-        stroke-opacity=".5"
+        strokeOpacity=".5"
         transform="matrix(1 0 0 -1 212.932 98)"
       />
       <circle
         r="97.5"
         stroke="currentColor"
-        stroke-opacity=".5"
+        strokeOpacity=".5"
         transform="matrix(1 0 0 -1 148.283 98)"
       />
       <circle
         r="97.5"
         stroke="currentColor"
-        stroke-opacity=".5"
+        strokeOpacity=".5"
         transform="matrix(1 0 0 -1 230.377 98)"
       />
       <circle
         r="97.5"
         stroke="currentColor"
-        stroke-opacity=".5"
+        strokeOpacity=".5"
         transform="matrix(1 0 0 -1 164.702 98)"
       />
       <circle
         r="97.5"
         stroke="currentColor"
-        stroke-opacity=".5"
+        strokeOpacity=".5"
         transform="matrix(1 0 0 -1 246.796 98)"
       />
     </svg>
